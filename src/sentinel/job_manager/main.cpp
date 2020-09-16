a56e46473e57930ef5685efce507fc5330643a98
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-09-16 03:55:01 -0500

    Set up main

--- src/sentinel/job_manager/main.cpp
+++ src/sentinel/job_manager/main.cpp
@@ -1,16 +1,16 @@
 #include <mpi.h>
 #include <basket.h>
-#include <symbios/server/daemon.h>
-#include <symbios/server/server.h>
-#include <symbios/common/configuration_manager.h>
+#include <sentinel/common/daemon.h>
+#include <sentinel/job_manager/server.h>
+#include <sentinel/common/configuration_manager.h>
 
 int main(int argc, char* argv[]){
     MPI_Init(&argc,&argv);
     MPI_Barrier(MPI_COMM_WORLD);
-    if(argc > 1) SYMBIOS_CONF->CONFIGURATION_FILE=argv[1];
-    BASKET_CONF->BACKED_FILE_DIR=SYMBIOS_CONF->SERVER_DIR;
+    if(argc > 1) SENTINEL_CONF->CONFIGURATION_FILE=argv[1];
+    BASKET_CONF->BACKED_FILE_DIR=SENTINEL_CONF->JOBMANAGER_DIR;
     CharStruct log = "./single_node_symbios_server.log";
-    auto daemon = basket::Singleton<symbios::Daemon<symbios::Server>>::GetInstance(log);
+    auto daemon = basket::Singleton<common::Daemon<sentinel::job_manager::server>>::GetInstance(log);
     daemon->Run();
     return 0;
 }
