cc066ee117c76f5d8755916495f2222e3eeb870b
lukemartinlogan <lukemartinlogan@gmail.com>
2020-09-16 17:07:36 -0500

    Uncomment WorkerManagerStats

--- include/sentinel/common/data_structures.h
+++ include/sentinel/common/data_structures.h
@@ -59,7 +59,7 @@ typedef struct ResourceAllocation {
     }
 } ResourceAllocation;
 
-/*typedef struct WorkerManagerStats {
+typedef struct WorkerManagerStats {
     CharStruct id_; // for file io, the "id_" is the filename; for object store io, the "id_" is the key.
     size_t position_; // read/write start position
     char* buffer_;  // data content
@@ -74,31 +74,6 @@ typedef struct ResourceAllocation {
         num_tasks_queued_ = num_tasks_queued;
         num_tasks_exec_ = num_tasks_assigned - num_tasks_queued_;
         thrpt_kops = num_tasks_exec_ / thrpt_kops;
-    }
-} WorkerManagerStats;*/
-
-typedef struct WorkerManagerStats {
-    CharStruct id_; // for file io, the "id_" is the filename; for object store io, the "id_" is the key.
-    size_t position_; // read/write start position
-    char* buffer_;  // data content
-    size_t data_size_;
-    uint16_t storage_index_;
-
-    /*Define the default, copy and move constructor*/
-    WorkerManagerStats(): id_(), position_(0), buffer_(NULL), storage_index_(),data_size_(){}
-    WorkerManagerStats(const Data &other): id_(other.id_), position_(other.position_), buffer_(other.buffer_),
-                                           storage_index_(other.storage_index_),data_size_(other.data_size_) {}
-    WorkerManagerStats(ResourceAllocation &other): id_(other.id_), position_(other.position_), buffer_(other.buffer_),
-                                                   storage_index_(other.storage_index_),data_size_(other.data_size_) {}
-
-    /*Define Assignment Operator*/
-    WorkerManagerStats &operator=(const ResourceAllocation &other){
-        id_ = other.id_;
-        position_ = other.position_;
-        buffer_ = other.buffer_;
-        data_size_ = other.data_size_;
-        storage_index_ = other.storage_index_;
-        return *this;
     }
 } WorkerManagerStats;
 
