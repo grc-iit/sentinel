b1de025c5cd3a6229d40c3e18b7ec4cc8b2d57a2
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-09-17 18:20:50 -0500

    JobManager Server:   - Added TerminateJob   - Modified some of the arguments to RPC calls   - Implemented spawning and terminating WorkManagers   - Modified Wormanager Stats to work with ordered_map for O(1) performance

--- include/sentinel/job_manager/Server.h
+++ include/sentinel/job_manager/Server.h
@@ -16,24 +16,30 @@
 #include <common/data_structure.h>
 #include <future>
 #include <common/data_structure.h>
+#include <sentinel/worker_manager/client.h>
+#include <basket/common/singleton.h>
 
-typedef uint32_t workmanager_id, task_id;
+typedef uint32_t job_id, workmanager_id, task_id;
 
 namespace sentinel::job_manager{
     class Server {
     private:
         std::shared_ptr<RPC> rpc;
         std::unordered_map<workmanager_id, WorkerManagerStats> loadMap;
+        std::map<WorkerManagerStats, workmanager_id> reversed_loadMap;
+        std::mutex mtx_loadmap;
+
         std::unordered_map<workmanager_id, std::vector<task_id>> taskMap;
         std::unordered_map<task_id, std::pair<workmanager_id, task_id>> destinationMap;
-        /**
-         * TODO: have a reverse ordered_map by load
-         * - when u update the stats u also update this map.
-         * - One Next task u just refer directly to this map and not iterate.
-         */
-        std::shared_ptr<Job> job;
-        workmanager_id FindMinLoad();
+
+        std::mutex mtx_allocate;
+        std::unordered_map<workmanager_id, CharStruct> available_workermanagers;
+        std::unordered_map<job_id, std::vector<workmanager_id>> used_resources;
+        std::unordered_map<workmanager_id, job_id> reversed_used_resources;
+
+        std::unordered_map<job_id, std::shared_ptr<Job>> jobs;
         bool SpawnWorkerManagers(ResourceAllocation &resourceAllocation);
+        bool TerminateWorkerManagers(ResourceAllocation &resourceAllocation);
 
         void RunInternal(std::future<void> futureObj);
     public:
@@ -44,20 +50,29 @@ namespace sentinel::job_manager{
             auto basket=BASKET_CONF;
             rpc=basket::Singleton<RPCFactory>::GetInstance()->GetRPC(BASKET_CONF->RPC_PORT);
             std::function<bool(uint32_t)> functionSubmitJob(std::bind(&sentinel::job_manager::Server::SubmitJob, this, std::placeholders::_1));
+            std::function<bool(uint32_t)> functionTerminateJob(std::bind(&sentinel::job_manager::Server::TerminateJob, this, std::placeholders::_1));
             std::function<bool(uint32_t,WorkerManagerStats&)> functionUpdateWorkerManagerStats(std::bind(&sentinel::job_manager::Server::UpdateWorkerManagerStats, this, std::placeholders::_1, std::placeholders::_2));
             std::function<std::pair<bool, WorkerManagerStats>(uint32_t)> functionGetWorkerManagerStats(std::bind(&sentinel::job_manager::Server::GetWorkerManagerStats, this, std::placeholders::_1));
-            std::function<std::pair<uint32_t, uint32_t>(uint32_t)> functionGetNextNode(std::bind(&sentinel::job_manager::Server::GetNextNode, this, std::placeholders::_1));
+            std::function<std::pair<uint32_t, uint32_t>(uint32_t, uint32_t)> functionGetNextNode(std::bind(&sentinel::job_manager::Server::GetNextNode, this, std::placeholders::_1, std::placeholders::_2));
             std::function<bool(ResourceAllocation&)> functionChangeResourceAllocation(std::bind(&sentinel::job_manager::Server::ChangeResourceAllocation, this, std::placeholders::_1));
             rpc->bind("SubmitJob", functionSubmitJob);
+            rpc->bind("TerminateJob", functionTerminateJob);
             rpc->bind("UpdateWorkerManagerStats", functionUpdateWorkerManagerStats);
             rpc->bind("GetWorkerManagerStats", functionGetWorkerManagerStats);
             rpc->bind("GetNextNode", functionGetNextNode);
             rpc->bind("ChangeResourceAllocation", functionChangeResourceAllocation);
+
+            int i = 0;
+            for(auto&& node: SENTINEL_CONF->WORKERMANAGER_LISTS){
+                available_workermanagers.insert(std::make_pair(i, node));
+                i++;
+            }
         }
         bool SubmitJob(uint32_t jobId);
+        bool TerminateJob(uint32_t jobId);
         bool UpdateWorkerManagerStats(uint32_t workerManagerId, WorkerManagerStats &stats);
         std::pair<bool, WorkerManagerStats> GetWorkerManagerStats(uint32_t workerManagerId);
-        std::pair<workmanager_id, task_id> GetNextNode(uint32_t currentTaskId);
+        std::pair<workmanager_id, task_id> GetNextNode(uint32_t workermanagerId, uint32_t currentTaskId);
         bool ChangeResourceAllocation(ResourceAllocation &resourceAllocation);
     };
 }
--- src/sentinel/job_manager/server.cpp
+++ src/sentinel/job_manager/server.cpp
@@ -13,27 +13,60 @@ void sentinel::job_manager::Server::RunInternal(std::future<void> futureObj) {
 
 bool sentinel::job_manager::Server::SubmitJob(uint32_t jobId){
     auto classLoader = ClassLoader();
-    job = classLoader.LoadClass<Job>(jobId);
-    ResourceAllocation defaultResourceAllocation = SENTINEL_CONF->DEFAULT_RESOURCE_ALLOCATION;
-    /*
-     * Generate default ResourceAllocation
-     */
-    SpawnWorkerManagers(defaultResourceAllocation);
-    /*
-     * Something has to happen here with the dag
-     */
-    /**
-     * TODO: allocate the first node source to the allocated worker manager.
-     *
-     */
+    std::shared_ptr<Job> job = classLoader.LoadClass<Job>(jobId);
+    jobs.insert(std::make_pair(jobId, job));
 
+    ResourceAllocation defaultResourceAllocation = SENTINEL_CONF->DEFAULT_RESOURCE_ALLOCATION;
+    defaultResourceAllocation.jobId = jobId;
+
+    used_resources.insert(std::make_pair(jobId, std::vector<workmanager_id>()));
+    SpawnWorkerManagers(defaultResourceAllocation);
+
+    auto workermanager_client = basket::Singleton<sentinel::worker_manager::Client>::GetInstance();
+    workmanager_id workermanager = used_resources.at(jobId).at(0);
+
+    uint32_t collector_id = job->GetCollectorId();
+    workermanager_client->AssignTask(workermanager, collector_id);
+    //Lets ensure that load map is not empty
+    WorkerManagerStats wms = WorkerManagerStats();
+    UpdateWorkerManagerStats(workermanager, wms);
+}
+
+bool sentinel::job_manager::Server::TerminateJob(uint32_t jobId){
+    auto workermanager_client = basket::Singleton<sentinel::worker_manager::Client>::GetInstance();
+
+    auto possible_job = used_resources.find(jobId);
+    if (possible_job == used_resources.end()) return false;
+
+    auto workermanagers_used = used_resources.at(jobId);
+
+    for(auto&& node: workermanagers_used){
+        mtx_loadmap.lock();
+        WorkerManagerStats reverse_lookup = loadMap.at(node);
+        loadMap.erase(node);
+        reversed_loadMap.erase(reverse_lookup);
+        mtx_loadmap.unlock();
+
+        reversed_used_resources.erase(node);
+        available_workermanagers.insert(std::make_pair(node, SENTINEL_CONF->WORKERMANAGER_LISTS[node]));
+
+        if(!workermanager_client->FinalizeWorkerManager(node)) return false;
+    }
+    used_resources.erase(jobId);
 }
 
 bool sentinel::job_manager::Server::UpdateWorkerManagerStats(uint32_t workerManagerId, WorkerManagerStats &stats){
-
+    mtx_loadmap.lock();
     auto possible_load = loadMap.find(workerManagerId);
-    if (possible_load == loadMap.end()) loadMap.insert(std::pair<workmanager_id, WorkerManagerStats>(workerManagerId, stats));
-    else possible_load->second = stats;
+    if (possible_load == loadMap.end()){
+        loadMap.insert(std::pair<workmanager_id, WorkerManagerStats>(workerManagerId, stats));
+    }
+    else {
+        possible_load->second = stats;
+        reversed_loadMap.erase(stats);
+    }
+    reversed_loadMap.insert(std::pair<WorkerManagerStats, workmanager_id>(stats, workerManagerId));
+    mtx_loadmap.unlock();
     return true;
 }
 
@@ -43,23 +76,21 @@ std::pair<bool, WorkerManagerStats> sentinel::job_manager::Server::GetWorkerMana
     return std::pair<bool, WorkerManagerStats>(true, possible_load->second);
 }
 
-std::pair<workmanager_id, task_id> sentinel::job_manager::Server::GetNextNode(uint32_t currentTaskId){
-    /**
-     * use ordered_map to do O(1) traversal to get the least node load.
-     */
+std::pair<workmanager_id, task_id> sentinel::job_manager::Server::GetNextNode(uint32_t workermanagerId, uint32_t currentTaskId){
     auto possible_destination = destinationMap.find(currentTaskId);
     workmanager_id currentWorkermanagerId = possible_destination->second.first;
     //If we dont have a current destination, or the destination is over a certain fullness
     if (possible_destination == destinationMap.end() ||
         loadMap.at(currentWorkermanagerId).num_tasks_queued_ > SENTINEL_CONF->MAX_LOAD) {
         //find a new destination
-        workmanager_id newWorkermanager = FindMinLoad();
+        workmanager_id newWorkermanager = reversed_loadMap.begin()->second;
         task_id newTask;
         if(possible_destination == destinationMap.end()){
             //What is the id of the next task
-            newTask = job->GetNextTaskId(currentTaskId);
+            job_id current_job = reversed_used_resources.at(workermanagerId);
+            newTask = jobs.at(current_job)->GetNextTaskId(currentTaskId);
             destinationMap.insert(std::pair<task_id, std::pair<workmanager_id, task_id>>(currentTaskId,
-                                                                                         std::pair<workmanager_id, task_id>(newWorkermanager, newTask)));
+                    std::pair<workmanager_id, task_id>(newWorkermanager, newTask)));
         }
         else {
             newTask = possible_destination->second.second;
@@ -70,41 +101,63 @@ std::pair<workmanager_id, task_id> sentinel::job_manager::Server::GetNextNode(ui
 }
 
 bool sentinel::job_manager::Server::ChangeResourceAllocation(ResourceAllocation &resourceAllocation){
-    return SpawnWorkerManagers(resourceAllocation);
+    if( resourceAllocation.num_nodes_ > 0) return SpawnWorkerManagers(resourceAllocation);
+    else if( resourceAllocation.num_nodes_ < 0) return TerminateWorkerManagers(resourceAllocation);
+    return true;
 }
 
 bool sentinel::job_manager::Server::SpawnWorkerManagers(ResourceAllocation &resourceAllocation) {
-    MPI_Comm taskManagerComm;
-    MPI_Info info;
-    MPI_Info_create(&info);
     /**
      * TODO: Maintain current node , proc within node and thread index within Server class
      * - On allocate u calculate the nodes based on list and given allocation size.
      * - Build a list of hosts and then call the mpi spawn.
-     */
+    */
 
+    MPI_Comm taskManagerComm;
+    MPI_Info info[resourceAllocation.num_nodes_];
+    char *cmds[resourceAllocation.num_nodes_];
+    char **spawn_argv[resourceAllocation.num_nodes_];
+    int np[resourceAllocation.num_nodes_];
+    int errcodes[resourceAllocation.num_nodes_];
+    for(int i=0; i < resourceAllocation.num_nodes_; i++){
+        mtx_allocate.lock();
+        auto allocated_workermanager = available_workermanagers.begin();
+        available_workermanagers.erase(available_workermanagers.begin());
+        mtx_allocate.unlock();
 
-    MPI_Info_set(info, "hostfile", SENTINEL_CONF->WORKERMANAGER_DINAMIC_HOSTFILE.c_str());
+        used_resources.at(resourceAllocation.jobId).emplace_back(allocated_workermanager->first);
+        reversed_used_resources.insert(std::make_pair(allocated_workermanager->first, resourceAllocation.jobId));
 
-    char** spawn_argv = static_cast<char **>(malloc(1 * sizeof(char *)));
-    spawn_argv[0] = (char*) SENTINEL_CONF->CONFIGURATION_FILE.c_str();
+        MPI_Info_create(&info[i]);
+        MPI_Info_set(info[i],"host","node1");
+        cmds[i] = { SENTINEL_CONF->WORKERMANAGER_EXECUTABLE.data()};
+        spawn_argv[i][0] = { SENTINEL_CONF->CONFIGURATION_FILE.data()}; //TODO: this might be one
+        np[i] = {1};
+    }
+    MPI_Comm_spawn_multiple(resourceAllocation.num_nodes_, cmds, spawn_argv, np, info, 0, MPI_COMM_WORLD, &taskManagerComm, errcodes );
 
-    // TODO: append new nodes to the hostfile
-
-    MPI_Comm_spawn(SENTINEL_CONF->WORKERMANAGER_EXECUTABLE.c_str(), spawn_argv, resourceAllocation.num_nodes_,
-                   info, 0, MPI_COMM_SELF, &taskManagerComm,
-                   MPI_ERRCODES_IGNORE);
-
-    //Merge the mpi_comm and maintain it, to be able to talk to them?
-    //TODO: is this really necessary, are we doing any mpi comm?
-
+    for(int i=0; i < resourceAllocation.num_nodes_; i++){
+        if( errcodes[i] != MPI_SUCCESS) return false;
+    }
     return true;
 }
 
-workmanager_id sentinel::job_manager::Server::FindMinLoad() {
-    /**
-     * TODO:this changes to the order 1 code.
-     */
-    auto min = *min_element(loadMap.begin(), loadMap.end(),[](const auto &l, const auto &r) { return l.second.num_tasks_queued_ < r.second.num_tasks_queued_; });
-    return min.first;
+bool sentinel::job_manager::Server::TerminateWorkerManagers(ResourceAllocation &resourceAllocation){
+    auto workermanager_client = basket::Singleton<sentinel::worker_manager::Client>::GetInstance();
+    for(int i=resourceAllocation.num_nodes_; i < 0; i++){
+        mtx_loadmap.lock();
+        workmanager_id workermanager_killed = reversed_loadMap.begin()->second;
+        reversed_loadMap.erase(reversed_loadMap.begin());
+        loadMap.erase(workermanager_killed);
+        mtx_loadmap.unlock();
+
+        available_workermanagers.insert(std::make_pair(workermanager_killed, SENTINEL_CONF->WORKERMANAGER_LISTS[workermanager_killed]));
+        auto list_workmanagers = used_resources.at(resourceAllocation.jobId);
+        list_workmanagers.erase(std::remove(list_workmanagers.begin(), list_workmanagers.end(), workermanager_killed), list_workmanagers.end());
+        reversed_used_resources.erase(workermanager_killed);
+
+        if(!workermanager_client->FinalizeWorkerManager(workermanager_killed)) return false;
+    }
+    return true;
 }
+
