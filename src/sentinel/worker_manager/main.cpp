2ac8ac6e4898a7d9a893dfd35d1664aa4e37e01a
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-25 13:42:39 -0500

    fixed byteflow_regulator test for calculating inflow and outflow rate.

--- src/sentinel/worker_manager/main.cpp
+++ src/sentinel/worker_manager/main.cpp
@@ -13,15 +13,16 @@ int main(int argc, char **argv) {
     MPI_Init(&argc,&argv);
     std::string conf;
     uint16_t port;
+    char name[MPI_MAX_PROCESSOR_NAME];
+    int len;
     WorkerManagerId id;
     if(argc >= 4) {
         conf = argv[1];
         port = atoi(argv[2]);
         id = atoi(argv[3]);
-        char name[MPI_MAX_PROCESSOR_NAME];
-        int len;
         MPI_Get_processor_name(name, &len);
-        printf("Worker Manager %d at %s Initialized with conf %s, port %d\n",id, name, conf.data(),port);
+        if(BASKET_CONF->MPI_RANK == 0)
+            printf("Worker Manager %d at %s Initialized with conf %s, port %d\n",id, name, conf.data(),port);
     }else{
         perror("Not enough arguments ./worker_manager <conf> <port> <worker_manager_id>\n");
         exit(EXIT_FAILURE);
@@ -33,6 +34,8 @@ int main(int argc, char **argv) {
     SENTINEL_CONF->WORKERMANAGER_ID = id;
     auto daemon = basket::Singleton<common::Daemon<sentinel::worker_manager::Server>>::GetInstance();
     daemon->Run();
+    if(BASKET_CONF->MPI_RANK == 0)
+        printf("Worker Manager %d at %s Finalized with conf %s, port %d\n",id, name, conf.data(),port);
     MPI_Finalize();
     return 0;
 }
