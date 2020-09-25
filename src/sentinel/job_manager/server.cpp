0c24d4a921220f2cc8beb39bdc3147dc34906444
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-25 03:15:06 -0500

    fixed byteflow_regulator test for calculating inflow and outflow rate.

--- src/sentinel/job_manager/server.cpp
+++ src/sentinel/job_manager/server.cpp
@@ -275,8 +275,8 @@ bool sentinel::job_manager::Server::SpawnWorkerManagers(ThreadId required_thread
 //        auto output = system(cmd);
 //        printf("%d",output);
 //
-        MPI_Info_set(info,"host", worker_resource.node_name_.data());
-        MPI_Info_set(info,"hostfile", SENTINEL_CONF->WORKERMANAGER_HOST_FILE.c_str());
+        //MPI_Info_set(info,"host", worker_resource.node_name_.data());
+        MPI_Info_set(info,"add-hostfile", SENTINEL_CONF->WORKERMANAGER_HOST_FILE.c_str());
         MPI_Info_set(info,"map-by", "node");
         char * mpi_argv[4];
         mpi_argv[0] = SENTINEL_CONF->CONFIGURATION_FILE.data();
